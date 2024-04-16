52c6095fa5fbc6c8ead493c70cbcbb81565b4a82
CodeLegg <michaelraymondlegg@gmail.com>
2024-04-16 12:01:38 +0100

    Fix scrolling issue in base.html and script.js

--- store/forms.py
+++ store/forms.py
@@ -1,16 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your username', 'class': 'input-field'})
-    )
-    password = forms.CharField(
-        max_length=100,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password', 'class': 'input-field'})
-    )
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(
@@ -23,3 +14,25 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken.")
+        return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match.")
+        return password2
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username', 'class': 'input-field'})
+    )
+    password = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password', 'class': 'input-field'})
+    )
