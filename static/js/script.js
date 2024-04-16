716a95947cd4d93e75bc877fcc7e9850e78e6302
CodeLegg <michaelraymondlegg@gmail.com>
2024-04-16 16:39:30 +0100

    Refactor URLs and authentication related code in store app

--- static/js/script.js
+++ static/js/script.js
@@ -228,26 +228,6 @@ const initSecondSlider = () => {
 
 window.addEventListener("load", initSecondSlider);
 
-/////////////////////////////////////////////////////////////
-
-
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const auth = document.querySelector(".auth");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
-sign_up_btn.addEventListener("click", () => {
-    auth.classList.add("sign-up-mode");
-});
-sign_in_btn.addEventListener("click", () => {
-    auth.classList.remove("sign-up-mode");
-});
-sign_up_btn2.addEventListener("click", () => {
-    auth.classList.add("sign-up-mode2");
-});
-sign_in_btn2.addEventListener("click", () => {
-    auth.classList.remove("sign-up-mode2");
-});
 
 ////////////////////////////////////////////////////////
 
--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
      path('allhomeandfurniture/', views.allhomeandfurniture, name='allhomeandfurniture'),
     # Login & Logout URL
-    path('login_or_register/', views.login_or_register, name='login_or_register'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
     path('logout/', views.logout_user, name='logout'),
 
 
