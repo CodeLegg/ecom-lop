65055a44b0bac2d1d5f1801d35046d1856214d78
CodeLegg <michaelraymondlegg@gmail.com>
2024-04-24 20:39:51 +0100

    Refactor ecom/settings.py and store/views.py to improve login and redirect URL handling

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,39 +1,29 @@
-
-import os
-import dj_database_url
-import django_heroku
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-72i101$cq_tis$m$4(p_9q7m-maba##j%+6f8w8urzdku0&*-x')
+SECRET_KEY = 'django-insecure-^m8(-!r*nmq1=_akp8n13ub1*%d4tv9d(16x5yn%(&(_#-5^i$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lotsofpresents.herokuapp.com', 'localhost']
-
-# Application definition
+ALLOWED_HOSTS = []
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 INSTALLED_APPS = [
+    'store',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
-    'whitenoise.runserver_nostatic',  # Whitenoise for serving static files
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Whitenoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -47,7 +37,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,27 +52,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-...
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static/']
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Media files
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
--- store/views.py
+++ store/views.py
@@ -124,7 +124,6 @@ def allbedroomfurniture(request):
 def login_user(request):
     if request.method == "POST":
         login_form = LoginForm(request.POST)
-        next_url = request.POST.get("next", None)
         if login_form.is_valid():
             username = login_form.cleaned_data["username"]
             password = login_form.cleaned_data["password"]
@@ -132,18 +131,16 @@ def login_user(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, "You have successfully logged in.")
-                if next_url:
-                    return redirect(next_url)
+                next_url = request.POST.get("next")
+                if next_url and is_safe_url(next_url, allowed_hosts=request.get_host()):
+                    return HttpResponseRedirect(next_url)
                 else:
                     return redirect("home")
             else:
                 messages.warning(request, "Unsuccessful login. Please try again.")
     else:
         login_form = LoginForm()
-        next_url = request.GET.get("next", None)  # Check if next parameter is in GET data
-    return render(request, "login.html", {"login_form": login_form, "next": next_url})
-
-
+    return render(request, "login.html", {"login_form": login_form})
 
 def register_user(request):
     if request.method == "POST":
@@ -164,9 +161,7 @@ def register_user(request):
                 messages.success(
                     request, "You've successfully signed-up and signed-in."
                 )
-                # Redirect the user to the page they were trying to access before registration
-                next_url = request.session.get('next_url', '/')
-                return redirect(next_url)
+                return redirect("home")
             else:
                 # Something went wrong with authentication
                 messages.warning(request, "Failed to sign you in. Please try again.")
@@ -184,7 +179,6 @@ def register_user(request):
                 messages.warning(request, "Registration failed. Please try again.")
     else:
         registration_form = RegistrationForm()
-
     return render(request, "register.html", {"registration_form": registration_form})
 
 def logout_user(request):
