67ca07186a401ed0d0c4c5d7c0c687d42e4ece9a
CodeLegg <michaelraymondlegg@gmail.com>
2024-05-15 11:47:53 +0100

    Update cart functionality and quantity selection in product.html, cart.py, and views.py

--- cart/views.py
+++ cart/views.py
@@ -7,37 +7,45 @@ from django.contrib import messages
 
 # Create your views here.
 def cart_summary(request):
-	# Get the cart
-	cart = Cart(request)
-	cart_products = cart.get_prods
-	quantity_range = range(1, 11)  # Range from 1 to 10
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantity_range = range(1, 11)  # Range from 1 to 10
+    quantities = cart.get_quants
+    return render(
+        request,
+        "cart_summary.html",
+        {"cart_products": cart_products, "quantity_range": quantity_range, "quantities": quantities},
+    )
 
-	return render(request, "cart_summary.html", {"cart_products":cart_products, 'quantity_range': quantity_range})
 
 def cart_add(request):
-	# Get the cart
-	cart = Cart(request)
-	# test for POST
-	if request.POST.get('action') == 'post':
-		# Get stuff
-		product_id = int(request.POST.get('product_id'))
+    # Get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get("action") == "post":
+        # Get stuff
+        product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
 
-		# lookup product in DB
-		product = get_object_or_404(Product, id=product_id)
-		
-		# Save to session
-		cart.add(product=product)
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
 
-		# Get Cart Quantity
-		cart_quantity = cart.__len__()
+        # Save to session
+        cart.add(product=product, quantity=product_qty)
 
-		# Return resonse
-		# response = JsonResponse({'Product Name: ': product.name})
-		response = JsonResponse({'qty': cart_quantity})
-		return response
-    
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
+
+        # Return resonse
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({"qty": cart_quantity})
+        return response
+
+
 def cart_delete(request):
     pass
+
 
 def cart_update(request):
     pass
