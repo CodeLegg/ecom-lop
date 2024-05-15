2fa197edb8fdfb0c4306b4eade21267ca098d024
CodeLegg <michaelraymondlegg@gmail.com>
2024-05-15 18:41:41 +0100

    Update cart functionality and styles in cart_summary.html, categories.css, product_page.css, and product.html templates

--- cart/views.py
+++ cart/views.py
@@ -44,7 +44,14 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get("action") == "post":
+        # Get stuff
+        product_id = int(request.POST.get("product_id"))
+        cart.delete(product=product_id)
+        
+        response = JsonResponse({"product": product_id})
+        return response
 
 
 def cart_update(request):
