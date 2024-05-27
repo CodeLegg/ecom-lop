9924d877abb1d8372fcea88bee23ef1f24c2bc93
CodeLegg <michaelraymondlegg@gmail.com>
2024-05-27 21:57:22 +0100

    Refactor JavaScript and templates for cart functionality and add search functionality intergrate ajax func on foo pages

--- static/js/cart_button_category.js
+++ static/js/cart_button_category.js
@@ -17,3 +17,4 @@ $(document).on('click', '.cart-btn', function (e) {
         }
     });
 });
+
--- store/urls.py
+++ store/urls.py
@@ -11,13 +11,20 @@ urlpatterns = [
     path("update_password/", views.update_password, name="update_password"),
     path("logout/", views.logout_user, name="logout"),
     # Product
-    path('product/<int:pk>/', views.product, name='product'),
-    path('category/<str:foo>/', views.category, name='category'),
-    path('category_children/<str:foo>/', views.category_children, name='category_children'),
-    path('level_two_categories/<str:foo>/', views.level_two_categories, name='level_two_categories'),
-    path('all_categories/<str:foo>/', views.all_categories, name='all_categories')
-
-
+    path("product_list/", views.productlistAjax, name="product_list"),
+    path("search_product/", views.search_product, name="search_product"),
+    path("product/<int:pk>/", views.product, name="product"),
+    path("category/<str:foo>/", views.category, name="category"),
+    path(
+        "category_children/<str:foo>/",
+        views.category_children,
+        name="category_children",
+    ),
+    path(
+        "level_two_categories/<str:foo>/",
+        views.level_two_categories,
+        name="level_two_categories",
+    ),
+    path("all_categories/<str:foo>/", views.all_categories, name="all_categories"),
     # Bedroom Category
-
 ]
