3703a9a37dafa0ac9be5f860c252ccfe07a00158
CodeLegg <michaelraymondlegg@gmail.com>
2024-06-20 17:25:41 +0100

    add env

---
+++ ecom/env.py
@@ -0,0 +1,4 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-72i101$cq_tis$m$4(p_9q7m-maba##j%+6f8w8urzdku0&*-x')
