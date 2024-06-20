ae3a56eeb53911b6391ba2bf51bccf8bb57d16dc
CodeLegg <michaelraymondlegg@gmail.com>
2024-06-20 17:48:08 +0100

    chore: Update SECRET_KEY handling in settings.py

--- env.py
+++ env.py
@@ -1,5 +1,4 @@
-import os
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY", "django-insecure-72i101$cq_tis$m$4(p_9q7m-maba##j%+6f8w8urzdku0&*-x"
-)
+
+SECRET_KEY = "django-insecure-72i101$cq_tis$m$4(p_9q7m-maba##j%+6f8w8urzdku0&*-x"
+
